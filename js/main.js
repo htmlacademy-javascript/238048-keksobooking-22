7b3a36e7cb9a8c35187dbdec6f8980c27e2efe36
irin <puhkaya@selectel.ru>
2021-02-02 15:41:12 +0300

    fix indents

--- js/main.js
+++ js/main.js
@@ -1,48 +1,48 @@
 function checkValues(a, b) {
-    const errorText = {
-        negative: 'Диапазон может быть только положительный!',
-        incorrectRange: 'Значение «до» меньшее, чем значение «от»!',
-        incorrectValues: 'Введите минимум 2 значения',
-    };
-    let error = '';
+  const errorText = {
+    negative: 'Диапазон может быть только положительный!',
+    incorrectRange: 'Значение «до» меньшее, чем значение «от»!',
+    incorrectValues: 'Введите минимум 2 значения',
+  };
+  let error = '';
 
-    if (!a || !b) {
-        error = errorText.incorrectValues;
-    } else if (a < 0 || b < 0) {
-        error = errorText.negative;
-    } else if (a > b) {
-        error = errorText.incorrectRange;
-    }
+  if (!a || !b) {
+    error = errorText.incorrectValues;
+  } else if (a < 0 || b < 0) {
+    error = errorText.negative;
+  } else if (a > b) {
+    error = errorText.incorrectRange;
+  }
 
-    return error;
+  return error;
 }
 
 function gatRandomInt(start, end) {
-    const error = checkValues(start, end);
-    if (error.length > 0) {
-        console.error(error);
-        return;
-    }
-    
-    if (start === end) return start;
-    let result = start + Math.random() * (end + 1 - start);
-    return Math.floor(result);
+  const error = checkValues(start, end);
+  if (error.length > 0) {
+    console.error(error);
+    return;
+  }
+
+  if (start === end) return start;
+  let result = start + Math.random() * (end + 1 - start);
+  return Math.floor(result);
 }
 
 function gatRandomFloat(start, end, simbolsAfterComma) {
-    let error = '';
-    let result;
+  let error = '';
+  let result;
 
-    if (!simbolsAfterComma) {
-        error = 'Укажите количество знаков после запятой!';
-    } else {
-        error = checkValues(start, end);
-    }
-    if (error.length > 0) {
-        console.error(error);
-        return;
-    }
-    result = start + Math.random() * (end - start);
-    if (start === end) result = start;
-    return result.toFixed(simbolsAfterComma);
+  if (!simbolsAfterComma) {
+    error = 'Укажите количество знаков после запятой!';
+  } else {
+    error = checkValues(start, end);
+  }
+  if (error.length > 0) {
+    console.error(error);
+    return;
+  }
+  result = start + Math.random() * (end - start);
+  if (start === end) result = start;
+  return result.toFixed(simbolsAfterComma);
 }
