a37b55f250fadcdddb104dbb42144736320466bc
irin <puhkaya@selectel.ru>
2021-02-02 14:58:31 +0300

    create checkValues function

--- js/main.js
+++ js/main.js
@@ -1,19 +1,29 @@
-function gatRandomInt(start, end) {
-    const errors = {
+function checkValues(a, b) {
+    const errorText = {
         negative: 'Диапазон может быть только положительный!',
         incorrectRange: 'Значение «до» меньшее, чем значение «от»!',
         incorrectValues: 'Введите минимум 2 значения',
     };
-    if (!start || !end) {
-        console.error(errors.incorrectValues);
-        return;
-    } else if (start < 0 || end < 0) {
-        console.error(errors.negative);
-        return;
-    } else if (start > end) {
-        console.error(errors.incorrectRange);
+    let error = '';
+
+    if (!a || !b) {
+        error = errorText.incorrectValues;
+    } else if (a < 0 || b < 0) {
+        error = errorText.negative;
+    } else if (a > b) {
+        error = errorText.incorrectRange;
+    }
+
+    return error;
+}
+
+function gatRandomInt(start, end) {
+    const error = checkValues(start, end);
+    if (error.length > 0) {
+        console.error(error);
         return;
     }
+    
     if (start === end) return start;
     start = Math.ceil(start);
     end = Math.floor(end);
